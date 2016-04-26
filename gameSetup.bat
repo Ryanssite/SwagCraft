f33e73c852d47ef23f69feb459d9567b9f9a4255
Ryanssite <ryanthepickleyou@gmail.com>
2016-04-25 21:08:48 -0400

    Create gameSetup.bat

---
+++ gameSetup.bat
@@ -0,0 +1,39 @@
+@echo off
+
+echo Seting Up...
+
+mkdir C:\Ryan
+mkdir C:\Ryan\Game
+
+mkdir C:\Ryan\Photos
+
+mkdir C:\Ryan\Lancher
+
+mkdir C:\Ryan\Resource_Packs
+
+cls
+
+echo Stage 1 complete. Press any key to start file transplant.
+
+pause>nul
+
+cd "C:\Ryan\Game"
+
+echo Copying GameScript1.js
+
+cls
+
+echo var Test = 9;>GameScript1.js
+
+cls
+
+echo Making Default Resource pack...
+
+cd "C:\Ryan\Resource_Packs"
+
+mkdir Default
+cd "Default"
+
+
+
+
